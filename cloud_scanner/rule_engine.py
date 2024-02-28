ede2a4ef599b84a3fb2c2c16aa107f9ea1b18b6a
Ikari <zachary@zachary-miller.com>
2024-02-28 00:04:54 -0500

    Minor edit, scrapped idea

--- cloud_scanner/rule_engine.py
+++ cloud_scanner/rule_engine.py
@@ -1,6 +1,7 @@
 import json
 from decorator import autolog
 from database_ops import fetch_entry_by_id
+from rule_runner import ec2_instance_check, s3_rule_check, rds_rule_check
 
 """
 
@@ -12,3 +13,17 @@ reprocess json obj, separating 3s and 2s, outputting len() as "Risk Score"
 Create simple REST API to allow zeroing in on what data class one would want.
 
 """
+
+
+def postprocessing():
+    ec2_json = ec2_instance_check()
+    s3_json = s3_rule_check()
+    rds_json = rds_rule_check()
+    try:
+        filtered_data = {
+            key: value
+            for key, value in s3_json.items()
+            if len(value.get("Violations", [])) > 2
+        }
+    except AttributeError as e:
+        print(f"Error processing data: {e}")
